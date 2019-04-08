fc046674202603f803ebd8c4fa14c25a0492dec8
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-04-08 15:27:14 -0600

    finished with mvp and stretch.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,65 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+    succeed,
+    fail,
+    repair,
+    get,
 };
 
-function succeed(item) {
-  return { ...item };
+function succeed( item ) {
+    let enhancement = 0;
+    
+    if ( item.enhancement ) {
+        if ( item.enhancement < 20 ) {
+            enhancement = item.enhancement + 1;
+        } else {
+            enhancement = 20;
+        }
+    }
+    
+    return { ...item, enhancement };
 }
 
-function fail(item) {
-  return { ...item };
+function fail( item ) {
+    
+    let durability = 0;
+    if ( item.enhancement && item.durability ) {
+        if ( item.enhancement < 15 ) {
+            durability = item.durability - 5;
+        } else if ( item.enhancement >= 15 ) {
+            durability = item.durability - 10;
+        }
+    }
+    
+    if ( durability < 0 ) {
+        durability = 0;
+    }
+    
+    let enhancement = 0;
+    if ( item.enhancement ) {
+        if ( item.enhancement > 16 ) {
+            enhancement = item.enhancement - 1;
+        } else {
+            enhancement = item.enhancement;
+        }
+    }
+    
+    return { ...item, enhancement, durability };
 }
 
-function repair(item) {
-  return { ...item };
+function repair( item ) {
+    return { ...item, durability: 100 };
 }
 
-function get(item) {
-  return { ...item };
+function get( item ) {
+    
+    let name = "";
+    if ( item.name ) {
+        if ( item.enhancement > 0 ) {
+            name = `[+${ item.enhancement }] ${ item.name }`;
+        } else {
+            name = item.name;
+        }
+    }
+    
+    return { ...item, name };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,110 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require( "./enhancer.js" );
 // test away!
+
+const testObject = {
+    durability:  27,
+    enhancement: 1,
+    name:        "Iron Sword"
+};
+
+const enhancementObjectMaxed = {
+    durability:  29,
+    enhancement: 20,
+    name:        "Wood Shield"
+};
+
+const blankTestObject = {};
+
+describe( "enhancer.js", () => {
+    
+    describe( "repair", () => {
+        it( "should return a new object with durability restored to 100",
+            () => {
+                
+                expect( enhancer.repair( testObject ) ).
+                    toEqual( { ...testObject, durability: 100 } );
+            } );
+        
+        it( "should return the blank object with durability set to 100", () => {
+            expect( enhancer.repair( blankTestObject ) ).
+                toEqual( { durability: 100 } );
+        } );
+    } );
+    
+    describe( "succeed", () => {
+        it( "should return a new object with enhancement plus one",
+            () => {
+                
+                expect( enhancer.succeed( testObject ) ).
+                    toEqual( {
+                        ...testObject,
+                        enhancement: testObject.enhancement + 1
+                    } );
+            } );
+        
+        it( "should return a new object with enhancement not changed and at" +
+            " level 20 ", () => {
+            expect( enhancer.succeed( enhancementObjectMaxed ) ).
+                toEqual( enhancementObjectMaxed );
+        } );
+        
+    } );
+    
+    describe( "fail", () => {
+        it( "should return a new object with durability  decreased by 10 and" +
+            " enhancement decreased by 1", () => {
+            expect( enhancer.fail( enhancementObjectMaxed ) ).
+                toEqual( {
+                    ...enhancementObjectMaxed,
+                    durability:  enhancementObjectMaxed.durability - 10,
+                    enhancement: enhancementObjectMaxed.enhancement - 1
+                } );
+        } );
+        
+        it( "should return a new object with durability decreased by 5 and" +
+            " enhancement the same.", () => {
+            expect( enhancer.fail( testObject ) ).
+                toEqual(
+                    { ...testObject, durability: testObject.durability - 5 } );
+        } );
+        
+        it( "should return a new object durability decreased by 10 and" +
+            " enhancement decreased by 0", () => {
+            expect( enhancer.fail( {
+                durability:  20,
+                enhancement: 15,
+            } ) ).toEqual( { durability: 10, enhancement: 15 } );
+        } );
+        
+        it( "should return a new object with durability equal to 0 and" +
+            " enhancement equal to 0", () => {
+            expect( enhancer.fail( blankTestObject ) ).
+                toEqual( { durability: 0, enhancement: 0 } );
+        } );
+        
+    } );
+    
+    describe( "get", () => {
+        it( "should return the item with the enhancment number between" +
+            " brackets then the name of the item.( [+5] item name )", () => {
+            expect( enhancer.get( testObject ) ).
+                toEqual( {
+                    ...testObject,
+                    name: `[+${ testObject.enhancement }] ${ testObject.name }`
+                } );
+        } );
+        
+        it( "should return the item as is with nothing changed because the" +
+            " enhancment level is 0.", () => {
+            expect( enhancer.get( {
+                enhancement: 0,
+                name:        "Iron Sword"
+            } ) ).toEqual( {
+                enhancement: 0,
+                name:        "Iron Sword"
+            } );
+        } );
+        
+    } );
+    
+} );
